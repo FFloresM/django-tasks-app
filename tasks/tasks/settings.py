ba7ac96a3abb61feeeadd644176927f8c69e90ed
Francisco <victoriafrancis2009@gmail.com>
2022-04-14 00:07:26 -0400

    task model

--- tasks/tasks/settings.py
+++ tasks/tasks/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasksapp.apps.TaskConfig',
 ]
 
 MIDDLEWARE = [
@@ -103,14 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
--- tasks/tasksapp/models.py
+++ tasks/tasksapp/models.py
@@ -1,3 +1,7 @@
+from enum import _auto_null, auto
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    text = models.CharField(max_length=200)
+    day = models.DateTimeField(auto_now=False)
+    reminder = models.BooleanField()
