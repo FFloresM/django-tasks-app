12e55ffd653093ddb15df68e1beb5a8f2af42fc2
Francisco <victoriafrancis2009@gmail.com>
2022-04-15 00:33:15 -0400

    index and detail views with templates

--- tasks/tasksapp/urls.py
+++ tasks/tasksapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'tasks'
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /tasks/5/
+    path('<int:task_id>/', views.detail, name='detail'),
 ]
--- tasks/tasksapp/views.py
+++ tasks/tasksapp/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from asyncio import tasks
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import Task
 
 
 def index(request):
-    return HttpResponse("Hello, you are at the tasks app index!")
+    list_of_tasks = Task.objects.all()
+    context = {
+        'list_of_tasks': list_of_tasks,
+    }
+    return render(request, 'tasksapp/index.html', context)
+
+def detail(request, task_id):
+    task = get_object_or_404(Task, pk=task_id)
+    return render(request, "tasksapp/detail.html", {'task': task})
