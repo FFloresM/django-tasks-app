6545b16bda632e8e97fb882ad86b49d48b8010ed
Francisco <victoriafrancis2009@gmail.com>
2022-04-14 23:13:06 -0400

    __str__ and admin site

--- tasks/tasksapp/models.py
+++ tasks/tasksapp/models.py
@@ -5,3 +5,6 @@ class Task(models.Model):
     text = models.CharField(max_length=200)
     day = models.DateTimeField(auto_now=False)
     reminder = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.text
